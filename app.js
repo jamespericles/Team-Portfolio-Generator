5d5aa0c325a183bd8038ba33b1b4f6c6d58f67fb
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-07-23 22:13:58 -0400

    added functionality for user to loop back and add multiple employees, intern class constructor now builds new intern and adds to the team array

--- app.js
+++ app.js
@@ -5,33 +5,58 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 const util = require("util");
+const render = require("./lib/htmlRenderer");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
+let team = [];
 // Array of questions related to an intern
 async function promptInternData() {
-  await inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is the intern's name?",
-    },
-    {
-      type: "input",
-      name: "ID",
-      message: "What is the intern's ID?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is the intern's email?",
-    },
-    {
-      type: "input",
-      name: "school",
-      message: "What school did the intern graduate from?",
-    },
-  ]);
+  let continueType;
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?",
+      },
+      {
+        type: "input",
+        name: "ID",
+        message: "What is the intern's ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school did the intern graduate from?",
+      },
+      {
+        type: "list",
+        name: "continue",
+        message: "Would you like to add another employee?",
+        choices: ["Yes", "No"],
+      },
+    ])
+    .then((answers) => {
+      const intern = new Intern(
+        answers.name,
+        answers.ID,
+        answers.email,
+        answers.school
+      );
+      team.push(intern);
+      continueType = answers.continue;
+      if (continueType === "Yes") {
+        promptEmployeeType();
+      } else if (continueType === "No") {
+        render();
+      }
+    });
 }
 
 // Array of questions related to an engineer
@@ -138,8 +163,6 @@ function generateHTML(answers) {}
 // }
 // const OUTPUT_DIR = path.resolve(__dirname, "output");
 // const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-// const render = require("./lib/htmlRenderer");
 
 // init();
 
